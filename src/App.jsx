77869c193f753b776205917df088c8b072b66f4e
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2024-07-15 16:48:12 -0300

    feat/add_loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Table, Navbar } from 'react-bootstrap';
 import { fetchData } from './services/api';
+import Loader from './components/Loader';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchData()
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -19,28 +27,32 @@ const App = () => {
         </Container>
       </Navbar>
       <Container>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>File Name</th>
-              <th>Text</th>
-              <th>Number</th>
-              <th>Hex</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((file, index) => (
-              file.lines.map((line, idx) => (
-                <tr key={`${index}-${idx}`}>
-                  <td>{file.file}</td>
-                  <td>{line.text}</td>
-                  <td>{line.number}</td>
-                  <td>{line.hex}</td>
-                </tr>
-              ))
-            ))}
-          </tbody>
-        </Table>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>File Name</th>
+                <th>Text</th>
+                <th>Number</th>
+                <th>Hex</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((file, index) => (
+                file.lines.map((line, idx) => (
+                  <tr key={`${index}-${idx}`}>
+                    <td>{file.file}</td>
+                    <td>{line.text}</td>
+                    <td>{line.number}</td>
+                    <td>{line.hex}</td>
+                  </tr>
+                ))
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Container>
     </div>
   );
